31eefdc4902e9d728615714670907739602b96c6
Matija Kočevar <matija.kocev@gmail.com>
2024-05-22 00:46:01 +0200

    Update bucket list component

--- src/app/features/bucket-list/bucket-list.component.ts
+++ src/app/features/bucket-list/bucket-list.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { BucketService } from '../../core/services/bucket.service';
+import { Bucket } from '../../core/models/bucket.model';
+import { CreateBucketComponent } from '../create-bucket/create-bucket.component';
 
 @Component({
   selector: 'app-bucket-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, CreateBucketComponent],
   templateUrl: './bucket-list.component.html',
-  styleUrl: './bucket-list.component.scss'
+  styleUrls: ['./bucket-list.component.scss']
 })
-export class BucketListComponent {
+export class BucketListComponent implements OnInit {
+  buckets: Bucket[] = [];
+  showCreateForm = false;
 
+  constructor(private bucketService: BucketService) { }
+
+  ngOnInit(): void {
+    console.log('BucketListComponent initialized');
+    this.bucketService.getBuckets().subscribe(data => {
+      this.buckets = data;
+    });
+  }
+
+  onBucketCreated(newBucket: Bucket) {
+    this.buckets.push(newBucket);
+    this.showCreateForm = false;
+  }
 }
