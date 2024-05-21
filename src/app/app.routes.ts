512a9d6f97c852516acd5478de94f54e5462a258
Matija Kočevar <matija.kocev@gmail.com>
2024-05-22 01:11:55 +0200

    Initial project setup (#2)
    * Add Bootstrap
    
    * Add initial bucket list component
    
    * Add initial bucket service
    
    * Reorganized project structure
    
    * Removed initial html
    
    * Update bucket service and create bucket model
    
    * Add initial create bucket component
    
    * Update bucket list component
    
    * Add EsLint
    
    * Add Prettier and format all files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { BucketListComponent } from './features/bucket-list/bucket-list.component';
+import { CreateBucketComponent } from './features/create-bucket/create-bucket.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: BucketListComponent },
+    { path: 'create', component: CreateBucketComponent },
+];
