3294e5e7cfc27041f43b7acd43efdb2e3c1e1b94
Matija Kočevar <matija.kocev@gmail.com>
2024-05-23 03:02:23 +0200

    Add SSR to Angular

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,8 +2,13 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withInterceptors, withFetch } from '@angular/common/http';
+import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-    providers: [provideRouter(routes), provideHttpClient(withInterceptors([]))],
+    providers: [
+        provideRouter(routes),
+        provideHttpClient(withInterceptors([]), withFetch()),
+        provideClientHydration()
+    ],
 };
