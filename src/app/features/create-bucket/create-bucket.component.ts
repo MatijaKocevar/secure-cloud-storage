9433371beb17e00e077508f27d99b8a5c0bed297
Matija Kočevar <matija.kocev@gmail.com>
2024-05-24 03:28:29 +0200

    All functionality working (#18)
    * Create initial bucket details component
    
    * Create initial file service
    
    * Add SSR to Angular
    
    * Update Bucket service to use api, implement CreateBucket
    
    * Update FileService to use api
    
    * Formatted unformated files
    
    * Update env apiUrl
    
    * Fix buckets getting created twice
    
    * Update File model for easier imports
    
    * Add LocationService and api
    
    * Update BucketDetails component to have only one delete button for files
    
    * Create pipe for display file size correctly
    
    * Update files list with columns "Last modified" and "Size"
    
    * Add api to get location by id
    
    * Update components to use locationId correctly
    
    * Add basic error handling to api endpoints
    
    * Add upload file logic and error handling
    
    * Create and implement modal component for confirming delete
    
    * Delete all files related to deleted bucket

--- src/app/features/create-bucket/create-bucket.component.ts
+++ src/app/features/create-bucket/create-bucket.component.ts
@@ -1,7 +1,10 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Bucket } from '../../core/models/bucket.model';
+import { BucketService } from '../../core/services/bucket/bucket.service';
+import { LocationService } from '../../core/services/location/location.service';
+import { BucketLocation } from '../../core/models/location.model';
 
 @Component({
     selector: 'app-create-bucket',
@@ -10,15 +13,42 @@ import { Bucket } from '../../core/models/bucket.model';
     templateUrl: './create-bucket.component.html',
     styleUrls: ['./create-bucket.component.scss'],
 })
-export class CreateBucketComponent {
+export class CreateBucketComponent implements OnInit {
     @Output() bucketCreated = new EventEmitter<Bucket>();
 
-    bucket: Bucket = { id: 0, name: '', location: '' };
-    locations = ['Location 1', 'Location 2', 'Location 3'];
+    bucket: Partial<Bucket> = { id: 0, name: '', locationId: 0 };
+    locations: BucketLocation[] = [];
+    errorMessage = '';
+
+    constructor(
+        private bucketService: BucketService,
+        private locationService: LocationService,
+    ) {}
+
+    ngOnInit() {
+        this.locationService.getLocations().subscribe({
+            next: (locations: BucketLocation[]) => {
+                this.locations = locations;
+            },
+            error: (error) => {
+                console.error('Error fetching locations', error);
+            },
+        });
+    }
 
     createBucket() {
-        this.bucket.id = Math.floor(Math.random() * 1000);
-        this.bucketCreated.emit(this.bucket);
-        this.bucket = { id: 0, name: '', location: '' };
+        console.log('CreateBucketComponent: createBucket called');
+        const observer = {
+            next: (createdBucket: Bucket) => {
+                this.bucketCreated.emit(createdBucket);
+                this.bucket = { id: 0, name: '', locationId: 0 };
+                this.errorMessage = '';
+            },
+            error: () => {
+                this.errorMessage = 'Error creating bucket';
+            },
+        };
+
+        this.bucketService.createBucket(this.bucket as Bucket).subscribe(observer);
     }
 }
