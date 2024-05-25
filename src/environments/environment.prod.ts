b39b1ea27f22930ef4a0dcbf40f4f4823a89f592
Matija Kočevar <matija.kocev@gmail.com>
2024-05-25 06:09:50 +0200

    Update prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: '/api',
+    apiUrl: '/projects/secure-cloud-storage/api',
     host: 'https://matijakocevar.com/projects/secure-cloud-storage',
     baseHref: '/projects/secure-cloud-storage/',
 };
